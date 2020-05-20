0cdd6239d210bb807c44b7f462a36acebb26ea52
Anna <an.zajac@outlook.com>
2020-05-20 13:21:34 +0200

    Presets and initial SidePane

--- src/main/scala/GameVectors.scala
+++ src/main/scala/GameVectors.scala
@@ -2,13 +2,15 @@ import scala.collection.mutable.ListBuffer
 
 class GameVectors() {
 
-  private var vectors = ListBuffer[Vector2D]()
+  private var vectors: ListBuffer[Vector2D] = ListBuffer[Vector2D]()
   private val rand = scala.util.Random
   var currentVector = new Vector2D(0, 0)
 
   def +(vector2D: Vector2D): Unit =  this.vectors += vector2D
 
   def -(vector2D: Vector2D): Unit = this.vectors -= vector2D
+
+  def addInitialVectors(listBuffer: ListBuffer[Vector2D]) = this.vectors=listBuffer
 
   def getList: List[Vector2D] = this.vectors.toList
 
