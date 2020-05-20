f53aeaa0d973f3cfdc1279598ec29c473fe93ee3
enkelian <aniano13@gmail.com>
2020-05-20 17:27:54 +0200

    responsibility repaired

--- src/main/scala/GameVectors.scala
+++ src/main/scala/GameVectors.scala
@@ -1,8 +1,11 @@
 import scala.collection.mutable.ListBuffer
 
 class GameVectors() {
-  private var vectors = ListBuffer[Vector2D]()
+  var vectors = ListBuffer[Vector2D]()
   private val rand = scala.util.Random
+
+  var generatedPoints: ListBuffer[Vector2D] = ListBuffer[Vector2D]()
+
   var currentVector = new Vector2D(0, 0)
 
   def +(vector2D: Vector2D): Unit = this.vectors += vector2D
@@ -11,18 +14,25 @@ class GameVectors() {
 
   def addInitialVectors(listBuffer: ListBuffer[Vector2D]): Unit = this.vectors = listBuffer
 
-  def getList: List[Vector2D] = this.vectors.toList
+  def getInitialList: List[Vector2D] = this.vectors.toList
+
+  def getGeneratedList: List[Vector2D] = this.generatedPoints.toList
+
+  def getAll: List[Vector2D] = this.vectors.appendAll(this.generatedPoints).toList
 
   def getRandomVector: Vector2D = this.vectors(rand.nextInt(this.vectors.size))
 
   def nextVector(fraction: Double): Vector2D = {
     var nextVector = this.getRandomVector
     nextVector = this.currentVector.getNextVector(nextVector, fraction)
-    this.vectors.addOne(nextVector)
+    this.generatedPoints.addOne(nextVector)
     this.currentVector = nextVector
     nextVector
   }
 
-  def clear(): Unit = this.vectors.clear()
+  def clear(): Unit = {
+    this.vectors.clear()
+    this.generatedPoints.clear()
+  }
 
 }
