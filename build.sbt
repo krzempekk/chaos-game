20124fff60bcec6b5a0e052eaa547515d50382f4
Kamil Krzempek <kamilkrzempek99@gmail.com>
2020-05-20 11:43:51 +0200

    scalafx dependency added

--- build.sbt
+++ build.sbt
@@ -3,3 +3,21 @@ name := "chaos-game"
 version := "0.1"
 
 scalaVersion := "2.13.2"
+
+libraryDependencies += "org.scalafx" %% "scalafx" % "14-R19"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-Xcheckinit", "-encoding", "utf8", "-feature")
+
+fork := true
+
+lazy val osName = System.getProperty("os.name") match {
+  case n if n.startsWith("Linux") => "linux"
+  case n if n.startsWith("Mac") => "mac"
+  case n if n.startsWith("Windows") => "win"
+  case _ => throw new Exception("Unknown platform!")
+}
+
+lazy val javaFXModules = Seq("base", "controls", "fxml", "graphics", "media", "swing", "web")
+libraryDependencies ++= javaFXModules.map(m =>
+  "org.openjfx" % s"javafx-$m" % "11" classifier osName
+)
--- src/main/scala/Vector2D.scala
+++ src/main/scala/Vector2D.scala
@@ -1,11 +1,11 @@
 class Vector2D(val x: Int, val y: Int) {
   def +(other: Vector2D) = new Vector2D(x + other.x, y + other.y)
   def unary_- = new Vector2D(-x, -y)
-  def -(other: Vector2D) = this + (-other)
+  def -(other: Vector2D): Vector2D = this + (-other)
   def *(other: Vector2D) = new Vector2D(x * other.x, y + other.y)
-  def <(other: Vector2D) = x < other.x && y < other.y
-  def >(other: Vector2D) = x > other.x && y > other.y
-  def <=(other: Vector2D) = this < other || this == other
+  def <(other: Vector2D): Boolean = x < other.x && y < other.y
+  def >(other: Vector2D): Boolean = x > other.x && y > other.y
+  def <=(other: Vector2D): Boolean = this < other || this == other
 
   override def equals(obj: Any): Boolean = obj match {
     case other: Vector2D => this.x == other.y && this.y == other.y
@@ -15,5 +15,4 @@ class Vector2D(val x: Int, val y: Int) {
   override def hashCode(): Int = this.x.hashCode() * 2137 + this.y * 13
 
   override def toString: String = "(" + this.x.toString + "," + this.y.toString +")"
-
 }
